480988e1180baedf6e22acd77a1d5448b7bbd1ce
nemoguigrat <sharos185@gmail.com>
2023-11-11 17:29:03 +0500

    cors

--- src/main/java/com/example/photographer/config/SecurityConfig.java
+++ src/main/java/com/example/photographer/config/SecurityConfig.java
@@ -147,9 +147,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
--- src/main/java/com/example/photographer/service/dto/activity/response/AdminActivityResponse.java
+++ src/main/java/com/example/photographer/service/dto/activity/response/AdminActivityResponse.java
@@ -1,4 +1,43 @@
 package com.example.photographer.service.dto.activity.response;
 
+import com.example.photographer.support.ShootingType;
+import com.example.photographer.support.domain.ShootingTime;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminActivityResponse {
+
+    Long locationId;
+
+    Long eventId;
+
+    Long zoneId;
+
+    String name;
+
+    String description;
+
+    LocalDateTime startTime;
+
+    LocalDateTime endTime;
+
+    Integer photographersCount;
+
+    Integer priority;
+
+    List<ShootingTime> shootingTime;
+
+    ShootingType shootingType;
+
+    String importantPersons;
+
+    String activityCode;
 }
