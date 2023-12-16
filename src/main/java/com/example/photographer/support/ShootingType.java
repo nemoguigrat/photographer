0c52df6ef075e38ea0c6b9636ca86e23897bd9e8
nemoguigrat <sharos185@gmail.com>
2023-12-16 17:55:52 +0500

    update docker

--- src/main/java/com/example/photographer/support/ShootingType.java
+++ src/main/java/com/example/photographer/support/ShootingType.java
@@ -1,4 +1,16 @@
 package com.example.photographer.support;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum ShootingType {
+    @JsonProperty("start")
+    START,
+    @JsonProperty("start")
+    END,
+    @JsonProperty("start")
+    ALL,
+    @JsonProperty("start")
+    START_END,
+    @JsonProperty("start")
+    ANY
 }
